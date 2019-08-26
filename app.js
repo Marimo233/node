d9104e51e10826ca190ed0208209e7188a7c4ffe
marimo <1072452597@qq.com>
2019-08-26 14:52:32 +0800

    feat

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const bodyParser = require('koa-bodyparser')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/Order')
 const UserModule=mongoose.model('User',{name:String,password:Number})
+//早餐模型
+const breakfastModule=mongoose.model('breakfast',{title:String,price:String})
+//晚餐模型
+const dinnerModule=mongoose.model('dinner',{title:String,price:String})
 app.use(bodyParser())
 //增加返回表单页面的路由login
 router.get ('/user', async (ctx,next)=>{
@@ -25,9 +29,13 @@ router.post('/user/login',async (ctx,next)=>{
 //处理提交的种类
 router.post('/submitkind',async (ctx,next)=>{
   let {breakfast,dinner}=ctx.request.body
-  // const User=new UserModule({name,password})
-  // const res=await User.save()
-  ctx.response.body={breakfast,dinner}
+  //保存早餐
+  const Breakfast=new breakfastModule({...breakfast})
+  const breakfastRes= await Breakfast.save()
+  //保存晚餐
+  const Dinner=new dinnerModule({...dinner})
+  const dinnerRes= await Dinner.save()
+  ctx.response.body={breakfastRes,dinnerRes}
 })
 
     app.use(router.routes()).listen(8000,()=>[
