23730c094f3ce1354def3870eab35ad9404e9861
marimo <1072452597@qq.com>
2019-08-26 14:27:38 +0800

    feta

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const app=new Koa()
 const bodyParser = require('koa-bodyparser')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/Order')
-const UserModule=mongoose.model('User',{name:String,id:number})
+const UserModule=mongoose.model('User',{name:String,password:Number})
 app.use(bodyParser())
-//增加返回表单页面的路由
+//增加返回表单页面的路由login
 router.get ('/user', async (ctx,next)=>{
   
   const data='<form action="/user/login" method="post"><input name="name" type="text" placeholder=""/><br/><input name="password" type="text" placeholder=""/><br/><button>GOOO</button></form>'
@@ -20,6 +20,13 @@ router.post('/user/login',async (ctx,next)=>{
   let {name,password}=ctx.request.body
   const User=new UserModule({name,password})
   const res=await User.save()
+  ctx.response.body=res
+})
+//处理提交的种类
+router.post('/submitkind',async (ctx,next)=>{
+  let {breakfast,breakfast}=ctx.request.body
+  // const User=new UserModule({name,password})
+  // const res=await User.save()
   ctx.response.body=res
 })
 
